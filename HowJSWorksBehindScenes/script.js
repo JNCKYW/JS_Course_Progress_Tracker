95031e582e19bb52c0e99b6528d475e3b2598576
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-05-08 19:08:25 +0200

    Update script.js

--- HowJSWorksBehindScenes/script.js
+++ HowJSWorksBehindScenes/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  function printAge() {
+    const output = `${firstName} You are ${age} years old born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      const firstName = `Michał`;
+      const str = `millenial ${firstName}`;
+      console.log(str);
+    }
+  }
+  printAge();
+  return age;
+}
+
+const firstName = `Janusz`;
+
+calcAge(1995);
