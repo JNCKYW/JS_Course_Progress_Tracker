6bdaa9e0ce14377e6427bf354abbd5d24df501a6
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-06-09 19:01:47 +0200

    Update script.js

--- Closer_Look_At_Functions/script.js
+++ Closer_Look_At_Functions/script.js
@@ -1,1 +1,48 @@
-'use strict';
+"use strict";
+
+/*
+const bookings = [];
+
+const createBooking = function (flightNum, numPassengers = 1, price = 600) {
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking(`LH123`);
+createBooking(`LH123`, 2, 400);
+createBooking(`LH123`, undefined, 1000);
+
+
+const flight = `LH234`;
+const jonas = {
+  name: `Janusz Wojciechowski`,
+  passportNum: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = `LH999`;
+  passenger.name = `Mr.` + passenger.name;
+
+  if (passenger.passportNum === 123456789) {
+    alert(`CHECKED IN`);
+  } else {
+    alert(`Wrong Passport!!!!`);
+  }
+};
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+const newPassport = function (person) {
+  person.passportNum = Math.trunc(Math.random() * 10000000000000);
+};
+
+newPassport(jonas);
+checkIn(flight, jonas);
+*/
