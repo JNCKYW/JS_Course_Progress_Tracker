2a724b474f17b754580f47d12b7c94b40ffc2531
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-07-20 22:53:01 +0200

    Update script.js

--- 13_Advanced_DOM/script.js
+++ 13_Advanced_DOM/script.js
@@ -389,3 +389,17 @@ slider();
 // const randomColor = `rgb(${randomInt(255)},${randomInt(255)},${randomInt(
 //   255
 // )})`;
+
+// document.addEventListener(`DOMContentLoaded`, function (e) {
+//   console.log(e);
+// });
+
+// window.addEventListener(`load`, function (e) {
+//   console.log(e);
+// });
+
+// window.addEventListener(`beforeunload`, function (e) {
+//   e.preventDefault();
+//   console.log(e);
+//   e.returnValue = ` `;
+// });
