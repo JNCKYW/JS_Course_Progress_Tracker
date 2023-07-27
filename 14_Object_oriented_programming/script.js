bf5aeee1ac3a78df4d22559fbd8c3dedc9a667d6
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-07-27 18:52:48 +0200

    Update script.js

--- 14_Object_oriented_programming/script.js
+++ 14_Object_oriented_programming/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+//////////////////////////////////////
+//CONSTRUCTOR FUNCTIONS
+
+const Person = function (firstName, birthYear) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  // NEVER DO THIS vvvv
+  // this.calcAge = function () {
+  //   console.log(2023 - this.birthYear);
+  // };
+};
+
+const janusz = new Person(`Janusz`, 1998);
+const matilda = new Person(`Matilda`, 1999);
+
+console.log(janusz);
+console.log(matilda);
+
+// 1. New {} is created
+// 2. function is called and this keyword = {}
+// 3. {} is linked to prototype
+// 4. function automatically return {}
+
+console.log(janusz instanceof Person);
