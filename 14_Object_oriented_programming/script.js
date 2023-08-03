3cf7d68a7119709ca03698c85da328b03aa079c1
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-08-03 20:05:28 +0200

    Update script.js

--- 14_Object_oriented_programming/script.js
+++ 14_Object_oriented_programming/script.js
@@ -1,9 +1,10 @@
 'use strict';
 
-//////////////////////////////////////
-//CONSTRUCTOR FUNCTIONS
+//################################
+// CONSTRUCTOR FUNCTIONS
+//################################
 
-const Person = function (firstName, birthYear) {
+/*const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
 
@@ -40,3 +41,83 @@ Person.prototype.species = `Homo Sapiens`;
 console.log(janusz.species);
 console.log(matilda.species);
 
+const h1 = document.querySelector(`h1`);
+
+console.dir(h1);
+*/
+
+//################################
+// ES6 CLASSES
+//################################
+
+class PersonCl {
+  constructor(firstName, birthYear) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    const now = new Date();
+    console.log(now.getFullYear() - this.birthYear);
+  }
+}
+
+PersonCl.prototype.greeter = function () {
+  console.log(`Hey, how are you ${this.firstName}`);
+};
+
+const janusz = new PersonCl(`Janusz`, 1998);
+
+console.log(janusz);
+janusz.calcAge();
+janusz.greeter();
+
+//1. Classes are not hoisted - we cant use them before we declare them
+//2. Classes are first-class citizens - we can pass them info functions and get them from functions
+//3. Classes are executed in strict mode always
+
+//################################
+// SETTER AND GETTER
+//################################
+
+// Every object in JS have setter and getter property which is function that get or set value
+
+const account = {
+  owner: `Janusz`,
+  movements: [100, 50, 60, 79],
+
+  get latest() {
+    return this.movements.slice().pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+//getter
+console.log(account.latest);
+
+//setter
+account.latest = 50;
+console.log(account.movements);
+
+//Getters and setters can be usefull in data validations
+
+//################################
+// HOW TO ADD STATIC METHODS THAT ARE NOT INHERITED
+//################################
+
+PersonCl.hey = function () {
+  console.log(`hey there`);
+};
+PersonCl.hey();
+
+/*Or You can just add method like normall one into PersonCl but add
+//static keyword before it:
+//
+//
+//static hey(){
+  console.log(`hey there`);
+}
+*/
