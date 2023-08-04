7b5c6fbcb4d7539d81dc4c748ae0a1703732486d
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-08-04 23:47:06 +0200

    Update challanges.js

--- 14_Object_oriented_programming/challanges.js
+++ 14_Object_oriented_programming/challanges.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Car = function (brand, speed) {
+/*const Car = function (brand, speed) {
   this.brand = brand;
   this.speed = speed;
 };
@@ -23,3 +23,39 @@ Mercedes.accelerate();
 
 BMW.brake();
 Mercedes.brake();
+*/
+
+//##################################
+// ES6 Classes
+//##################################
+
+class CarCl {
+  constructor(brand, speed) {
+    this.brand = brand;
+    this.speed = speed;
+  }
+  accelerate() {
+    this.speed += 10;
+    console.log(`${this.brand} going at ${this.speed} km/h`);
+  }
+  brake() {
+    this.speed -= 5;
+    console.log(`${this.brand} going at ${this.speed} km/h`);
+  }
+
+  get speedUS() {
+    return this.speed / 1.6;
+  }
+
+  set speedUS(speed) {
+    this.speed = speed * 1.6;
+  }
+}
+
+const Ford = new CarCl(`Ford`, 100);
+
+// Ford.accelerate();
+// Ford.brake();
+console.log(Ford.speedUS);
+Ford.speedUS = 100;
+console.log(Ford.speed);
