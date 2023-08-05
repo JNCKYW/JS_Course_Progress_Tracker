098e3286575ea61e7607dc18aa4dd02b8353f6f6
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-08-05 15:22:29 +0200

    Update challanges.js

--- 14_Object_oriented_programming/challanges.js
+++ 14_Object_oriented_programming/challanges.js
@@ -23,7 +23,7 @@ Mercedes.accelerate();
 
 BMW.brake();
 Mercedes.brake();
-*/
+
 
 //##################################
 // ES6 Classes
@@ -59,3 +59,47 @@ const Ford = new CarCl(`Ford`, 100);
 console.log(Ford.speedUS);
 Ford.speedUS = 100;
 console.log(Ford.speed);
+*/
+
+//##################################
+// CONSTRUCTOR FUNCTIONS INHERITAGE
+//##################################
+
+const Car = function (brand, speed) {
+  this.brand = brand;
+  this.speed = speed;
+};
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(`${this.brand} going at ${this.speed} km/h`);
+};
+
+Car.prototype.brake = function () {
+  this.speed -= 5;
+  console.log(`${this.brand} going at ${this.speed} km/h`);
+};
+
+//new child constructor function
+const EV = function (brand, speed, charge) {
+  Car.call(this, brand, speed);
+  this.charge = charge;
+};
+
+//linking Car prototype to EV prototype
+EV.prototype = Object.create(Car.prototype);
+
+//adding new methods to EV
+EV.prototype.chargeBattery = function (battery) {
+  this.charge = battery;
+};
+
+EV.prototype.accelerate = function () {
+  this.speed += 20;
+  this.charge -= 1;
+  console.log(
+    `${this.brand} going at ${this.speed} km/h, with a charge of ${this.charge}%`
+  );
+};
+
+const tesla = new EV(`Tesla`, 120, 23);
