93e3f74155aca33fc2c2ade56213f3896d0e3376
Janusz Wojciechowski <131555509+JNCKYW@users.noreply.github.com>
2023-08-13 21:07:47 +0200

    Update script.js

--- 15_MAPTY_APP/script.js
+++ 15_MAPTY_APP/script.js
@@ -1,12 +1,41 @@
-'use strict';
+"use strict";
 
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
-const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
-const inputType = document.querySelector('.form__input--type');
-const inputDistance = document.querySelector('.form__input--distance');
-const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--cadence');
-const inputElevation = document.querySelector('.form__input--elevation');
+const form = document.querySelector(".form");
+const containerWorkouts = document.querySelector(".workouts");
+const inputType = document.querySelector(".form__input--type");
+const inputDistance = document.querySelector(".form__input--distance");
+const inputDuration = document.querySelector(".form__input--duration");
+const inputCadence = document.querySelector(".form__input--cadence");
+const inputElevation = document.querySelector(".form__input--elevation");
+
+//############################################################
+//HOW TO USE GEOLOCATION + LEAFLET EXTERNAL LIBRARY
+//############################################################
+
+navigator.geolocation.getCurrentPosition(
+  function (position) {
+    const latitude = position.coords.latitude;
+    const longtitude = position.coords.longitude;
+    console.log(
+      `https://www.google.com/maps/@${latitude},${longtitude},17z?hl=pl-PL&entry=ttu`
+    );
+
+    const map = L.map("map").setView([latitude, longtitude], 13);
+
+    L.tileLayer("https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    L.marker([latitude, longtitude])
+      .addTo(map)
+      .bindPopup("A pretty CSS3 popup.<br> Easily customizable.")
+      .openPopup();
+  },
+  function () {
+    alert(`Could not get your position :(`);
+  }
+);
